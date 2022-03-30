3fba0622f1f93cf7456a4d0b2d0e43e6422ec1af
DESKTOP-RA01494\KGA_10 <webqud30@gmail.com>
2022-03-30 15:55:59 +0900

    configure comfile header

---
+++ socoban/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once //헤더파일은 단 한번만.!
+//순환 참조?? : 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h> //논리 대수를 저장하는 타입;
